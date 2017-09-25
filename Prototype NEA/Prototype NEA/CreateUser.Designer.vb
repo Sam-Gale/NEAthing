aec0ffe97a195e4f13c3c782c91a480e0d813116
8SGA1408 <8SGA1408@barton.ac.uk>
2017-09-25 11:33:32 +0100

    saved

---
+++ Prototype NEA/Prototype NEA/CreateUser.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CreateUser
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NewUsernameBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PasswordBox = New System.Windows.Forms.TextBox()
+        Me.RepeatPasswordBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Username:"
+        '
+        'NewUsernameBox
+        '
+        Me.NewUsernameBox.Location = New System.Drawing.Point(124, 46)
+        Me.NewUsernameBox.Name = "NewUsernameBox"
+        Me.NewUsernameBox.Size = New System.Drawing.Size(100, 20)
+        Me.NewUsernameBox.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Enter Password:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 146)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(84, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Enter Password:"
+        '
+        'PasswordBox
+        '
+        Me.PasswordBox.Location = New System.Drawing.Point(124, 116)
+        Me.PasswordBox.Name = "PasswordBox"
+        Me.PasswordBox.Size = New System.Drawing.Size(100, 20)
+        Me.PasswordBox.TabIndex = 4
+        '
+        'RepeatPasswordBox
+        '
+        Me.RepeatPasswordBox.Location = New System.Drawing.Point(124, 143)
+        Me.RepeatPasswordBox.Name = "RepeatPasswordBox"
+        Me.RepeatPasswordBox.Size = New System.Drawing.Size(100, 20)
+        Me.RepeatPasswordBox.TabIndex = 5
+        '
+        'CreateUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.RepeatPasswordBox)
+        Me.Controls.Add(Me.PasswordBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.NewUsernameBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "CreateUser"
+        Me.Text = "CreateUser"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NewUsernameBox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PasswordBox As TextBox
+    Friend WithEvents RepeatPasswordBox As TextBox
+End Class
